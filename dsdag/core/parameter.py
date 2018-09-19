f8fe99a7c3b1a67f5e749ddf6a28bbe8fdb99ca8
Stuart, Morgan S (Genworth) <morgan.stuart@genworth.com>
2018-09-19 09:38:18 -0400

    BaseParameter's value is now a property with get/set
    Override decode and encode for get/set. Actual getter and
    setter has some light wrapper code to not re-call these
    methods if they've been called before. Saves time in hashing
    which speeds up DAG build greatly for some parameters.

--- dsdag/core/parameter.py
+++ dsdag/core/parameter.py
@@ -6,44 +6,60 @@ class _UnsetParameter(object):
 
 class BaseParameter(object):
     _reserved_kw = ('name',)
-    def __init__(self, default=_UnsetParameter,
+    def __init__(self, default=_UnsetParameter(),
                  help_msg=None):
         self.is_unset = True
         self.help_msg = help_msg
         self.default_value = default
         self.uid = str(uuid.uuid4())
-        self.set_value(default)
+        #self.set_value(default)
+        self.value = default
+        self._cached_decoded = None
 
     def __eq__(self, other):
         if isinstance(other, BaseParameter) or issubclass(type(other), BaseParameter):
-            return self.value == other.value
+            return self._value == other._value
         else:
-            return self.value == other
+            return self._value == other
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     def __hash__(self):
-        return hash((type(BaseParameter), self.value))
+        return hash((type(BaseParameter), self._value))
 
     def set_value(self, v):
         self.is_unset = isinstance(v, _UnsetParameter)
         if not self.is_unset:
-            self.value = self.parse_value(v)
+            self._value = self.encode_value(v)
         else:
-            self.value = v
+            self._value = v
 
-    def parse_value(self, v):
-        return v
+    def get_value(self):
+        self.is_unset = isinstance(self._value, _UnsetParameter)
+        if not self.is_unset:
+            if self._cached_decoded is None:
+                self._cached_decoded = self.decode_value(self._value)
+            return self._cached_decoded
+        else:
+            return self._value
+
+    value = property(fget=get_value, fset=set_value, fdel=None, doc=None)
+
+    def encode_value(self, v):
+        return  v
+
+    def decode_value(self, enc_v):
+        return enc_v
 
     def value_repr(self):
-        return str(self.value)
+        return str(self._value)
 
     def __repr__(self):
         try:
             type_name = type(self.value).__name__
         except:
-            type_name = str(type(self.value))
+            type_name = str(type(self._value))
 
         return "(%s)%s" % (type_name, self.value_repr())
 
@@ -78,17 +94,46 @@ class UnhashableParameter(BaseParameter):
         return hash(self)
 
 class DatetimeParameter(BaseParameter):
-    def parse_value(self, v):
+    def encode_value(self, v):
         return pd.to_datetime(v)
 
 class SQLQueryParameter(BaseParameter):
     def value_repr(v):
         return "'%s ...'" % str(v)[:10].strip()
 
-import interactive_data_tree as idt
-class RepoTreeParameter(UnhashableParameter):
-    def parse_value(self, v):
-        return idt.RepoTree(v)
+class RepoTreeParameter(BaseParameter):
+
+    def __hash__(self):
+        return hash((type(BaseParameter), self._value))
+
+    def encode_value(self, v):
+        import interactive_data_tree as idt
+        if isinstance(v, idt.RepoLeaf):
+            parent_rt_path = v.parent_repo.idr_prop['repo_root']
+            leaf_name = v.name
+            return (parent_rt_path, leaf_name)
+        elif isinstance(v, idt.RepoTree):
+            parent_rt_path = v.idr_prop['repo_root']
+            return parent_rt_path
+        else:
+            raise ValueError("%s expects %s, got %s"
+                             % (self.__class__.__name__,
+                                idt.RepoLeaf.__name__,
+                                str(type(v))))
+
+
+    def decode_value(self, enc_v):
+        import interactive_data_tree as idt
+        #if len(self._value) == 2:
+        if isinstance(self._value, tuple):
+            parent_rt_path, leaf_name = self._value
+            return idt.RepoTree(parent_rt_path)[leaf_name]
+        else:
+            parent_rt_path = self._value
+            return idt.RepoTree(parent_rt_path)
+
+
+
 
 class DataFrameParameter(BaseParameter):
     pass
