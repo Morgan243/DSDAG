c7e333a863b904c2e32b154a0d0fa115d8c60731
Stuart, Morgan S (Genworth) <morgan.stuart@genworth.com>
2018-09-19 09:40:59 -0400

    Passthrough Provider and DataTreeLoader
    Pass through returns the object it was given, override this
    if your opp simply needs to setup requirements and have the
    value returned.
    
    DataTreeLoader uses the new RepoTreePrameter to load data from
    the tree.

--- dsdag/ext/data_provider.py
+++ dsdag/ext/data_provider.py
@@ -1,7 +1,19 @@
 #import luigi
 from dsdag.core.op import OpVertex
-from dsdag.core.op import BaseParameter
+from dsdag.core.parameter import BaseParameter, RepoTreeParameter
+
 import gnwdata as gd
+class Passthrough(OpVertex):
+    _never_cache = True
+    def _node_color(self):
+        return '#eded5c'
+
+    def _node_shape(self):
+        return 'cylinder'
+
+    def run(self, o):
+        return o
+
 
 class BaseDataProvider(OpVertex):
     #param = luigi.Parameter()
@@ -44,3 +56,29 @@ class BaseDataProvider(OpVertex):
     def output(self):
         pass
         #return self.
+
+
+class DataTreeLoader(OpVertex):
+    rt_leaf = RepoTreeParameter()
+    _never_cache = True
+
+
+    def _node_color(self):
+        return '#ed9d5c'
+
+    def _node_shape(self):
+        return 'cylinder'
+
+    def run(self):
+        return self.rt_leaf.load()
+        #import interactive_data_tree as idt
+        #if isinstance(self.rt_leaf, (list, tuple)):
+        #    if any(not isinstance(rl, (idt.RepoLeaf)) for rl in self.rt_leaf):
+        #        raise ValueError("One of the objects provided in the set is not an idt.RepoLeaf")
+        #    return [l.load() for l in self.rt_leaf]
+        #elif isinstance(self.rt_leaf, (idt.RepoLeaf)):
+        #    return self.rt_leaf.load()
+        #else:
+        #    msg = ("%s can't handle rt_leaf parameter of type %s"
+        #           % (self.__class__.__name__, type(self.rt_leaf)))
+        #    raise ValueError(msg)
