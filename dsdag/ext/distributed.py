ecd043f722821d15de2752ab97dd316678dd9365
Stuart, Morgan S (Genworth) <morgan.stuart@genworth.com>
2019-01-15 20:38:02 -0500

    Parallel op returns tuple; init helper function

--- dsdag/ext/distributed.py
+++ dsdag/ext/distributed.py
@@ -32,16 +32,10 @@ class DaskParallel(OpVertex):
         return self.inst_op.requires()
 
     def run(self, *args, **kwargs):
-        return dask.delayed(self.inst_op.run)(*args, **kwargs)
+        return (self.inst_op, dask.delayed(self.inst_op.run)(*args, **kwargs))
 
     def get_name(self):
-        #orig_name = super(DaskParallel, self).get_name()
-        try:
-            orig_name = self.parallel_op.get_name()
-            return "DaskParallel_%s" % orig_name
-        except:
-            return super(DaskParallel, self).get_name()
-
+        return self.parallel_op.get_name()
 
 class DaskCollect(OpVertex):
     """
@@ -56,12 +50,27 @@ class DaskCollect(OpVertex):
         return '#2fbc2d'
 
     def run(self, *ops):
+        self.ops = list()
+        self.delays = list()
+        for _op, _delay in ops:
+            self.ops.append(_op)
+            self.delays.append(_delay)
+
         compute_kwargs = dict(scheduler=self.scheduler,
                               num_workers=self.num_workers)
         if self.dask_progress_bar:
             with ProgressBar():
-                results = dask.compute(*ops, **compute_kwargs)
+                results = dask.compute(*self.delays, **compute_kwargs)
         else:
-            results = dask.compute(*ops, **compute_kwargs)
+            results = dask.compute(*self.delays, **compute_kwargs)
 
         return results
+
+def DaskParallelOps(ops=None, kw_ops=None, scheduler='threads', num_workers=2):
+    # can't do both
+    assert ops is not None or kw_ops is not None
+    p_ops = [DaskParallel(parallel_op=o)
+                for o in ops]
+    c_op = DaskCollect(scheduler=scheduler, num_workers=num_workers)(*p_ops)
+    c_op.unpack_output = True
+    return c_op
