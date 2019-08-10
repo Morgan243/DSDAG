de91dfe90551f82ecb0ceecf119e8a9de7742099
Morgan Stuart <morgansstuart243@gmail.com>
2019-08-10 18:50:32 -0400

    OpK and OpParent init
    Effort to keep inherited clutter out of the namespace of implemented Ops by moving metadata into
    an OpKernel (naming? OpK). OpParent is meant for inheriting to get user-level functionality where
    needed, which can be automatically done when using the new decorator style generator opvertex2.
    This reduces clutter, is more flexible than OO style alone, and gets all features/functionality
    through a single decorator. Needs to be used with DAG2 currently.

--- dsdag/core/op.py
+++ dsdag/core/op.py
@@ -8,6 +8,7 @@ import time
 import logging
 import inspect
 import pandas as pd
+import types
 
 from dsdag.core.parameter import BaseParameter, UnhashableParameter
 import collections
@@ -442,6 +443,260 @@ def opattr(default=attr.NOTHING, validator=None,
     return attr.ib(default=default, validator=validator, repr=True, cmp=True, hash=None,
                    init=init, convert=None, metadata=metadata, type=type, converter=converter,
                    factory=factory, kw_only=kw_only)
+
+@attr.s(cmp=False)
+class OpK(object):
+    run_callable = attr.ib()
+    requires_callable = attr.ib()
+
+    dag = attr.ib(None)
+    never_cache = attr.ib(False)
+    ops_to_upack = attr.ib(set())
+    unpack_output = attr.ib(False)
+    downstream = attr.ib(dict())
+
+    closure_map = dict()
+
+    def __attrs_post_init__(obj):
+
+        fields = attr.fields(obj.__class__)
+        obj.parameters_ = {f.name: getattr(obj, f.name) for f in fields}
+        obj.param_hashable = tuple([(k, v if isinstance(v, collections.Hashable) else str(uuid4()))
+                                    for k, v in obj.parameters_.items()])
+
+        obj.set_requires(obj.requires_callable)
+
+        obj.cacheable = not obj.never_cache
+        # only partially implemented in past versions, stub out for now
+        obj.runtime_parameters = dict()
+
+        return obj
+
+    @staticmethod
+    def is_op(obj):
+        return hasattr(obj, 'opk') and isinstance(obj.opk, OpK)
+
+    @staticmethod
+    def get_maybe_op_opk(obj):
+        if isinstance(obj, OpK):
+            return obj
+        elif OpK.is_op(obj):
+            return obj.opk
+        else:
+            return None
+
+    def get_requires(self, parent):
+        return getattr(parent, self.requires_callable_name, self.requires_callable)
+
+    def get_parameters(self):
+        return self.parameters_
+
+    def is_unpack_required(self, op):
+        return op in self.ops_to_upack
+
+    def set_downstream(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+            self.downstream[k] = v
+
+        return self
+
+    def apply(opk, *args, **kwargs):
+        if len(kwargs) == 0 and len(args) == 0:
+            return opk
+        elif len(kwargs) != 0:
+            from dsdag.ext.misc import VarOp, InputOp
+            # var and input are basically the same?
+            auto_op = lambda _obj: VarOp(obj=_obj) if not isinstance(_obj, collections.Hashable) else InputOp(obj=_obj)
+            req_ret = kwargs
+            req_hash = hash(tuple((k, OpK.get_maybe_op_opk(v) if OpK.is_op(v) or isinstance(v, OpK) else auto_op(v))
+                        for k, v in kwargs.items()))
+        elif len(args) != 0:
+            from dsdag.ext.misc import VarOp, InputOp
+            # var and input are basically the same?
+            auto_op = lambda _obj: VarOp(obj=_obj) if not isinstance(_obj, collections.Hashable) else InputOp(obj=_obj)
+            req_ret = list(args)
+            req_ret = [OpK.get_maybe_op_opk(a) if OpK.is_op(a) or isinstance(a, OpK) else auto_op(a)
+                       for a in req_ret]
+            req_hash = hash(tuple(req_ret))
+
+        else:
+            msg = "Mix of *args and **kwargs not supported (yet?)"
+            raise ValueError(msg)
+
+        for r in req_ret:
+            if hasattr(r, 'downstream'):
+                opk.set_downstream(**r.downstream)
+
+        if req_hash not in opk.__class__.closure_map:
+            def closure(self):
+                return req_ret
+            opk.__class__.closure_map[req_hash] = closure
+
+        # Use descriptor protocol: https://docs.python.org/2/howto/descriptor.html
+        #self.requires = closure.__get__(self)
+        #self.req_hash = req_hash
+        #opk._req_hashable = req_hash
+        #opk.requires = opk.__class__.closure_map[opk._req_hashable].__get__(opk)
+        opk.set_requires(opk.__class__.closure_map[req_hash].__get__(opk),
+                         hash_of_requires=req_hash,
+                         name_of_method='_auto_requires')
+
+        return opk
+
+    def set_requires(self, requires_callable, hash_of_requires=None,
+                     name_of_method='requires'):
+        self.requires_callable = requires_callable
+        self.requires_callable_name = name_of_method if name_of_method is not None else None
+
+        if self.requires_callable is not None:
+            if self.requires_callable_name is None:
+                self.requires_callable_name = self.requires_callable.__name__
+
+            if hash_of_requires is None:
+                try:
+                    self.req_hashable = inspect.getsource(self.requires_callable)
+                except:
+                    print("Cannot get source of requires: %s" % str(self.requires_callable))
+                    self.req_hashable = uuid4()
+        elif hash_of_requires is None:
+            self.req_hashable = ('requires_nothing',)
+        elif hash_of_requires is not None:
+            self.req_hashable = hash_of_requires
+
+    def __hash__(self):
+        self._op_hash = hash((type(self), self.param_hashable, self.req_hashable))
+        return self._op_hash
+
+    def _set_dag(self, dag):
+        from dsdag.core.dag import DAG
+        if not isinstance(dag, DAG) or not issubclass(type(dag), DAG):
+            msg = "Expected a DAG object or derived, got %s" % str(type(dag))
+            raise ValueError(msg)
+        self.dag = dag
+
+    def get_name(self):
+        #return self._name if self._name is not None else self.unique_cls_name
+        return self._name if self._name is not None else self.__class__.__name__
+
+    @staticmethod
+    def from_callable(callable,
+                      input_arguments=[0],
+                      callable_name=None):
+        """
+        Generate a class from a callable that can be used as an Op in a DAG.
+
+        :param callable:
+        :param input_arguments:
+        :param callable_name:
+        :return:
+        """
+        input_arguments = list() if input_arguments is None else input_arguments
+
+        ###
+        # NAME
+        if callable_name is not None:
+            pass
+        elif hasattr(callable, '__name__'):
+            callable_name = callable.__name__
+        elif hasattr(callable, '__class__'):
+            callable_name = callable.__class__.__name__
+        else:
+            callable_name = str(callable)
+
+        try:
+            sig = inspect.signature(callable)
+        except ValueError as e:
+            #print("Cannot obtain signature of %s" % callable_name)
+            sig = None
+
+        _attrs = dict()
+        if sig is not None:
+            for p_name, p in sig.parameters.items():
+                _attr = attr.ib(default=p.default, init=True,
+                                kw_only=p.kind == p.KEYWORD_ONLY)
+                _attrs[p_name] = _attr
+
+        _op = attr.make_class(callable_name, _attrs, bases=(OpParent,), cmp=False)
+
+        if sig is not None:
+            pos_inputs = [ia for ia in input_arguments if isinstance(ia, int)]
+            #kw_inputs = [ia for ia in input_arguments if ia not in pos_inputs]
+            def _run(s, *args, **kwargs):
+                _kwargs = {k: kwargs.get(k, getattr(s, k))
+                           for i, (k, v) in enumerate(sig.parameters.items())
+                            if i not in pos_inputs}
+                if len(args) != len(pos_inputs):
+                    print("Appears to be missing positional arguments")
+                    print("Expected %d args" % len(pos_inputs))
+                    print("But Got %d args" % len(args))
+                #_kwargs.update({k:kwargs[k] for k in kw_inputs })
+                #_args = [a for i, a in enumerate(args)]
+
+                return callable(*args, **_kwargs)
+
+        else:
+            def _run(s, *_args, **_kwargs): return callable(*_args, **_kwargs)
+
+        _op.run = types.MethodType(_run, _op)
+        #_op.opk = OpK(_run, None)
+        _op = opvertex2(_op, name=True)
+
+        return _op
+
+
+class OpParent(object):
+    def __call__(self, *args, **kwargs):
+        self.opk = self.opk.apply(*args, **kwargs)
+        return self
+
+    def __repr__(self):
+        params = self.opk.get_parameters()
+        repr = ", ".join("%s=%s" % (str(k), str(params[k]))
+                        for k in sorted(params.keys()))
+        if self._name is not None:
+            repr += ', name=\'' + self._name + '\''
+        return self.__class__.__name__ + "(" + repr + ")"
+
+    def __hash__(self):
+        return hash(self.opk)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def _set_dag(self, dag):
+        self.opk._set_dag(dag)
+
+    def get_name(self):
+        return getattr(self, '_name', 'NONAME')
+
+    def requires(self):
+        return dict()
+
+
+def opvertex2(cls, run_method='run', requires_method='requires',
+              unpack_run_return=False, ops_to_unpack=tuple(),
+              name=True):
+    if isinstance(name, bool) and name:
+        cls._name = opattr(cls.__name__, init=True, kw_only=True)
+
+    attr_cls = attr.s(cls, cmp=False, these=None)
+
+    if not issubclass(cls, OpParent):
+        attr_cls = attr.make_class(cls.__name__,
+                                   attrs=[],# attrs=attr_cls.__attrs_attrs__
+                                   #bases=(OpParent, attr_cls),
+                                   bases=(attr_cls, OpParent),
+                                   cmp=False,
+                                   )
+
+    attr_cls.opk = OpK(getattr(attr_cls, run_method),
+                       getattr(attr_cls, requires_method, None),
+                       unpack_output=unpack_run_return,
+                       ops_to_upack=ops_to_unpack)
+
+    return attr_cls
+
 
 def opvertex(cls):
     cls._name = opattr(cls.__name__, init=True, kw_only=True)
