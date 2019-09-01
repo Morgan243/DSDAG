cb082ae23254a5991fb735b4f5f749a60e965cdb
Morgan Stuart <morgansstuart243@gmail.com>
2019-09-01 19:08:57 -0400

    changes to get viz test in CI

--- tests/context.py
+++ tests/context.py
@@ -5,5 +5,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import logging
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
 
+import matplotlib
+matplotlib.use('agg')
+
 import dsdag
 from dsdag import core
